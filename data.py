1739b9852447d14630ec95d2096698ccff8ccca6
Egemen Erin <113554575+EgemenErin@users.noreply.github.com>
2024-05-24 11:58:17 +0200

    Add files via upload

--- data.py
+++ data.py
@@ -1,6 +1,11 @@
 import requests
 
-response = requests.get("https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=boolean")
+parameters = {
+    "amount": 10,
+    "type": "boolean",
+}
+
+response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
 question_data = data["results"]
